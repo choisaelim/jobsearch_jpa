682e999393bf6e42103899f9065d927cfcdd4547
choisaelim <choisaelim@gmail.com>
2021-01-23 23:33:53 +0900

    .

---
+++ job/src/main/java/com/search/job/domain/posts/Posts.java
@@ -0,0 +1,12 @@
+package com.search.job.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+
+public class Posts {
+
+}
